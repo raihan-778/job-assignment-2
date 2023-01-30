81c8b22a560126bc7153fd411bae974ab7acdcf2
Md.Raihan Uddin <raihanarif778@gmail.com>
2023-01-30 23:48:27 +0600

    innitial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Add toggle button for small screens
+console.log("javascript is conntected");
+// let menuToggle = document.querySelector(".menu-toggle");
+// let navLinks = document.querySelector(".nav-links");
+
+// menuToggle.addEventListener("click", () => {
+//   navLinks.classList.toggle("open");
+// });
+
+// code for  Carousel
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const pervBtn = document.getElementById("slide-arrow-prev");
+const nextBtn = document.getElementById("slide-arrow-next");
+
+nextBtn.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+  console.log("next btn clicked");
+});
+
+pervBtn.addEventListener("click", () => {
+  console.log("prev btn clicked");
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
